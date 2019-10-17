815797f7445b6f951cea40b4641a9869e4ba1da5
Serhii_Y <dj.ser961@gmail.com>
2019-10-18 00:47:53 +0300

    Updates

---
+++ precache-manifest.5ec1ab9dd45650015a7000508143de1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3a3b51263487783c31f167a968e205",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "8d07d9627f8c81e6ea9c",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "35b810c71c20ff2e4551",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.adb3f6ce.chunk.css"
+  },
+  {
+    "revision": "8d07d9627f8c81e6ea9c",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.c9632c0e.chunk.js"
+  },
+  {
+    "revision": "35b810c71c20ff2e4551",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.bcbf7e5f.chunk.js"
+  },
+  {
+    "revision": "e55f51874e9b7fc8b03d",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime~main.c691db01.js"
+  }
+]);
